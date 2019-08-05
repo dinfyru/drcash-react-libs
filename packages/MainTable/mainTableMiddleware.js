34dba1d8b9576b041bd5b20d0849f9e653eb8146
dinfyru <dinfyru@gmail.com>
2019-08-05 15:49:54 +0300

    mtMiddleware

---
+++ packages/MainTable/mainTableMiddleware.js
@@ -0,0 +1,43 @@
+import cloneDeep from 'lodash.clonedeep';
+import queryBuilder from 'query-string';
+import compact from 'lodash.compact';
+
+const REQUEST_REGEXP = /^MT_LIST@(.+)_REQUEST$/;
+const RSAA = '@@redux-api-middleware/RSAA';
+
+export default store => next => action => {
+  if (action[RSAA]) {
+    const newAction = { ...action };
+    // stop request for list if is loading
+    if (REQUEST_REGEXP.test(action[RSAA].types[0])) {
+      const {
+        mainTable: {
+          [action.reducer]: { isLoading, filtersValue }
+        }
+      } = store.getState();
+      if (isLoading) {
+        return false;
+      }
+      if (filtersValue && Object.keys(filtersValue).length) {
+        const compactedQuery = cloneDeep(filtersValue);
+        Object.entries(compactedQuery).forEach(([key, value]) => {
+          const arr = Array.isArray(value) ? value : [value];
+          compactedQuery[key] = compact(arr);
+        });
+        const queryString = queryBuilder.stringify(compactedQuery);
+
+        if (queryString.length) {
+          newAction[RSAA].endpoint = `${
+            newAction[RSAA].endpoint
+          }?${queryString}`;
+        }
+      }
+
+      delete newAction.reducer;
+    }
+
+    return next(newAction);
+  }
+
+  return next(action);
+};
